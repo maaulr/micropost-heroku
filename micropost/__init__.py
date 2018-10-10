c1f1750f25675d9bf25847362ee2b950dd0b78b3
maulr <maulana.auliar@gmail.com>
2018-10-10 21:23:35 +0700

    added flask migrate

--- micropost/__init__.py
+++ micropost/__init__.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import db
+from flask_migrate import Migrate
 from whitenoise import WhiteNoise
 
 app = Flask(__name__)
@@ -9,5 +10,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.wsgi_app = WhiteNoise(app.wsgi_app, root='/static')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from micropost import views
