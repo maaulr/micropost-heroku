ae44c8dd6590a0067cc027d19bcd5c9928ada272
maulr <maulana.auliar@gmail.com>
2018-10-10 20:58:03 +0700

    fix

--- micropost/__init__.py
+++ micropost/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import db
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'gantiinidiproduksi')
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', None)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
