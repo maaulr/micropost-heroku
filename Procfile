fd5efce0e964e9c499ceb35a0e2d0af989e9d7a8
maulr <maulana.auliar@gmail.com>
2018-10-10 21:11:33 +0700

    add Procfile edit init

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn micropost:app --log-file=-
--- micropost/__init__.py
+++ micropost/__init__.py
@@ -1,11 +1,13 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import db
+from whitenoise import WhiteNoise
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'gantiinidiproduksi')
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='/static')
 db = SQLAlchemy(app)
 
 from micropost import views
